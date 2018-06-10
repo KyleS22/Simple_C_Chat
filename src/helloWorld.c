e42c02400d5b578e8b41050853ef2ac87235a3cd
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2018-06-10 16:36:01 -0600

    Add basic project structure and hello world program for familiarizing with cmake

---
+++ src/helloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "simpleCChatConfig.h"
+
+int main(){
+	printf("Version %d.%d.%d\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+
+	printf("Hello World!\n");
+
+	return 0;
+}
