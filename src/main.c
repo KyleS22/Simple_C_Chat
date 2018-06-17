f24ee7a608419a82746cc3b6b97f8296e6fdeb32
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2018-06-17 14:54:26 -0600

    Added some test structure

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "simpleCChatConfig.h"
+#include "helloWorld.h"
+#include <pthread.h>
+int main(){
+	printf("Version %d.%d.%d\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+
+	printf("Hello World!\n");
+	
+	pthread_t thread1, thread2;
+	int *thr = malloc(sizeof(*thr));
+	int *thr2 = malloc(sizeof(*thr2));
+
+	*thr = 1;
+	*thr2 = 2;
+	
+	if(pthread_create(&thread1, NULL, thread, (void *) thr) != 0){
+		perror("Error Creating Thread");
+		exit(1);
+	}
+
+	if(pthread_create(&thread2, NULL, thread, (void *) thr2) != 0){
+		perror("Error Creating Thread");
+		exit(1);
+	}
+
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+
+	return 0;
+}
